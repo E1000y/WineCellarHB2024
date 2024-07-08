addc694e40cbd0c0b4c28b6dcb603434d379f5c8
E1000y <emilie.paniagua@gmail.com>
2024-07-08 16:40:25 +0200

    Add project files.

---
+++ Models/Bottle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Bottle
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string FullName{ get; set; }
+        public int VintageYear{ get; set; }
+        public int YearsOfKeep { get; set; }
+        public string DomainName { get; set; }
+        public DateOnly PeakInDate { get; set; }
+        public DateOnly PeakOutDate { get; set; }
+
+        public int GrapeVariety { get; set; }
+
+
+
+    }
+}
---
+++ WineCellarHB2024/Controllers/BottlesController.cs
@@ -0,0 +1,11 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WineCellarHB2024.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BottlesController : ControllerBase
+    {
+    }
+}
