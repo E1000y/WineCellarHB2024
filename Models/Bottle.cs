3c11d3218caefdb222454568ea82722d16d2ecc9
E1000y <emilie.paniagua@gmail.com>
2024-07-09 09:12:48 +0200

    change all items to nullable in bottle class except id of course

--- Models/Bottle.cs
+++ Models/Bottle.cs
@@ -9,32 +9,32 @@ namespace Models
     internal class Bottle
     {
         public int Id { get; set; }
-        public string Color { get; set; }
-        public string Name { get; set; }
-        public string FullName{ get; set; }
-        public int VintageYear{ get; set; }
-        public int YearsOfKeep { get; set; }
-        public string DomainName { get; set; }
-        public DateOnly PeakInDate { get; set; }
-        public DateOnly PeakOutDate { get; set; }
+        public string? Color { get; set; }
+        public string? Name { get; set; }
+        public string? FullName{ get; set; }
+        public int? VintageYear{ get; set; }
+        public int? YearsOfKeep { get; set; }
+        public string? DomainName { get; set; }
+        public DateOnly? PeakInDate { get; set; }
+        public DateOnly? PeakOutDate { get; set; }
 
-        public int GrapeVariety { get; set; }
+        public int? GrapeVariety { get; set; }
 
-        public int Tava { get; set; }
+        public int? Tava { get; set; }
 
-        public int Capacity { get; set; }
+        public int? Capacity { get; set; }
 
-        public string WineMakerName { get; set; }
+        public string? WineMakerName { get; set; } = string.Empty;
 
-        public string VintageName { get; set; }
+        public string? VintageName { get; set; }
 
-        public string Aroma { get; set; }
+        public string? Aroma { get; set; }
 
-        public int Price { get; set; }
+        public int? Price { get; set; }
 
-        public DateOnly PurchaseDate { get; set; }
+        public DateOnly? PurchaseDate { get; set; }
 
-        public string RelatedMeals { get; set; }
+        public string? RelatedMeals { get; set; }
 
     }
 }
