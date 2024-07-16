2c7610e53ea6e38c03767678a2a755df3148eba8
Vincent-TH <vincentthomas31@outlook.com>
2024-07-16 16:24:02 +0200

    Bottle Duplicate

---
+++ Models/DTOs/BottleDupDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs
+{
+    public class BottleDupDTO
+    {
+        public int? DrawerPosition { get; set; }
+        public int DrawerId { get; set; }
+    }
+}
